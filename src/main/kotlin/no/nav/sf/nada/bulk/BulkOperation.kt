0cb61688b472b1d86afbc51fb776b1a7f37b405b
Björn Hägglund <bjorn.hagglund@nav.no>
2024-11-06 13:12:14 +0100

    Update kubernetes paths

--- src/main/kotlin/no/nav/sf/nada/bulk/BulkOperation.kt
+++ src/main/kotlin/no/nav/sf/nada/bulk/BulkOperation.kt
@@ -10,16 +10,3 @@ object BulkOperation {
 
     var jobId: String = ""
 }
-
-/**
- *
- * POST /services/data/v57.0/jobs/query
-Content-Type: application/json
-Authorization: Bearer <access_token>
-
-{
-"operation": "query",
-"query": "SELECT Id, TAG_ActivityType__c, TAG_service__c, Subject, CreatedDate, LastModifiedDate, CRM_AccountOrgNumber__c, ActivityDate, TAG_AccountNAVUnit__c, TAG_AccountOrgType__c, TAG_UserNAVUnit__c, TAG_AccountParentId__c, TAG_AccountParentOrgNumber__c, TAG_Status__c, Type, CRM_Region__c, CRM_Unit__c, ReminderDateTime, RecordTypeId, StartDateTime, EndDateTime, EventSubtype, DurationInMinutes, WhatId, WhoId, TAG_IACaseNumber__c, IASubtheme__c, TAG_IACooperationId__c, RecordTypeName__c FROM Event",
-"contentType": "CSV"
-}
- */
